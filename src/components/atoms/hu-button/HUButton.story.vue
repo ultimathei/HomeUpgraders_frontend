c221bac6ae4ba885f6a2c33cf8e65cc98a3c9f96
ultimathei <krisztian.mate.design@gmail.com>
2022-09-25 14:37:55 +0100

    initial setup

---
+++ src/components/atoms/hu-button/HUButton.story.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+  import HUButton from './HUButton.vue'
+  const scenarios = [
+    {
+      label: 'Hello'
+    },
+  ]
+</script>
+
+<template>
+  <Story title="Atoms/HUButton">
+    <HUButton :label="scenarios[0].label"/>
+  </Story>
+</template>
---
+++ src/components/atoms/hu-button/HUButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+  const props = defineProps({
+    label: {
+      type: String,
+      default: 'Count'
+    }
+  })
+
+  let count = ref(0)
+  const increase = () => count.value++
+
+  // const emit = defineEmits(['click'])
+</script>
+
+<template>
+  <button @click="increase">{{label}} - {{count}}</button>
+</template>
