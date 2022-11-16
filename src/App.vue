82fe603f84db69fbfee4689329b466ae0d79a1ff
ultimathei <krisztian.mate.design@gmail.com>
2022-11-16 17:10:28 +0000

    removed unused sample comp

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,3 @@
 <template>
   <router-view name="main" />
 </template>
-
-<!-- <script setup lang="ts">
-  // import { onMounted, reactive } from 'vue'
-  // fetch the config json
-  // set css variables based on config json
-  // const config = {
-  //   colors: {
-  //     primary: '#6DA0A6',
-  //     secondary: '#D99B66',
-  //     black: '#2c2a27',
-  //     white: '#D9D9D9',
-  //   },
-  // }
-
-  // const state = reactive({
-  //   initialising: true,
-  //   rootStyles: {},
-  // })
-
-  // onMounted(() => {
-  //   state.rootStyles = {
-  //     '--hu-color--primary': config.colors.primary,
-  //     '--hu-color--secondary': config.colors.secondary,
-  //     '--hu-color--black': config.colors.black,
-  //     '--hu-color--white': config.colors.white,
-  //   }
-  // })
-</script> -->
