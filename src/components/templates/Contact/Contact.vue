32f1f7f582098e6d5b047a1f0e7a054269fd62f6
ultimathei <krisztian.mate.design@gmail.com>
2022-11-17 18:07:32 +0000

    contact blured video bg

--- src/components/templates/Contact/Contact.vue
+++ src/components/templates/Contact/Contact.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue'
 import SectionHeading from '@Atoms/Heading/Section/Section.vue'
+const props = defineProps({
+  visible: Boolean,
+})
+// pause video playback when offscreen
+// play when onscreen
+const video = ref(null)
+watch(
+  () => props.visible,
+  (visible) => {
+    if (!video.value) return
+    visible
+      ? (video.value as HTMLVideoElement).pause()
+      : (video.value as HTMLVideoElement).play()
+  }
+)
 </script>
 
 <template>
@@ -20,6 +36,22 @@ import SectionHeading from '@Atoms/Heading/Section/Section.vue'
       <span>Email us at</span>
       <a href="mailto:info@homeupgraders.co.uk">info@homeupgraders.co.uk</a>
     </address>
+    <video
+      id="contact-video-background"
+      ref="video"
+      width="100%"
+      height="100%"
+      autoplay
+      disablepictureinpicture
+      loop
+      muted
+      playsinline
+    >
+      <source
+        :src="`src/assets/contact_bg.mp4`"
+        type="video/mp4"
+      />
+    </video>
   </section>
 </template>
 
