2114ea9825f452da8617a618fd6b980e76e48f41
ultimathei <krisztian.mate.design@gmail.com>
2022-11-17 17:47:58 +0000

    minor style adjust

--- src/components/templates/Contact/Contact.vue
+++ src/components/templates/Contact/Contact.vue
@@ -8,6 +8,18 @@ import SectionHeading from '@Atoms/Heading/Section/Section.vue'
       text="Contact"
       :light="true"
     />
+    <article>
+      <p>
+        We find it easiest to communicate through <span>email</span> Please
+        write us a couple of lines about your job and we will get back to you in
+        no time.
+      </p>
+      <p><span>Photos</span>are always welcome 📸 😊</p>
+    </article>
+    <address>
+      <span>Email us at</span>
+      <a href="mailto:info@homeupgraders.co.uk">info@homeupgraders.co.uk</a>
+    </address>
   </section>
 </template>
 
