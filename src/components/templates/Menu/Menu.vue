3e510a9bc9aae43056162bce6968c841ad2fa445
ultimathei <krisztian.mate.design@gmail.com>
2022-11-17 03:02:22 +0000

    header bg; menu tabindex

--- src/components/templates/Menu/Menu.vue
+++ src/components/templates/Menu/Menu.vue
@@ -5,22 +5,22 @@
   >
     <ul>
       <li>
-        <a href="#section-home" @click.prevent="$emit('click', 'home')" :aria-selected="active === 'home'">
+        <a href="#section-home" @click.prevent="$emit('click', 'home')" :aria-selected="active === 'home'" :tabindex="open ? 0 : -1">
           Home
         </a>
       </li>
       <li>
-        <a href="#section-about" @click.prevent="$emit('click', 'about')" :aria-selected="active === 'about'">
+        <a href="#section-about" @click.prevent="$emit('click', 'about')" :aria-selected="active === 'about'" :tabindex="open ? 0 : -1">
           About
         </a>
       </li>
       <li>
-        <a href="#section-contact" @click.prevent="$emit('click', 'contact')" :aria-selected="active === 'contact'">
+        <a href="#section-contact" @click.prevent="$emit('click', 'contact')" :aria-selected="active === 'contact'" :tabindex="open ? 0 : -1">
           Contact
         </a>
       </li>
       <li>
-        <a href="#section-references" @click.prevent="$emit('click', 'references')" :aria-selected="active === 'references'">
+        <a href="#section-references" @click.prevent="$emit('click', 'references')" :aria-selected="active === 'references'" :tabindex="open ? 0 : -1">
           References
         </a>
       </li>
